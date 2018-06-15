7a0f5d845f545872cd7536a5d53deece9f8b2a36
庞界宇 <pang.jie.yu@outlook.com>
2018-06-15 18:20:29 +0800

    添加项目文件。

---
+++ 2015exam/MainWindow.xaml.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _2015exam
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<Student> students;
+        public MainWindow()
+        {
+            InitializeComponent();
+            students = new List<Student>();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            if(int.Parse(scoreTextBox.Text)>100 || int.Parse(scoreTextBox.Text)<0)
+            {
+                MessageBox.Show("成绩不符合要求", "错误");
+                scoreTextBox.Clear();
+            }
+            else
+            {
+                Student student = new Student(Int64.Parse(snoTextBox.Text), nameTextBox.Text, comboBox.Text, int.Parse(scoreTextBox.Text));
+                students.Add(student);
+                StringBuilder sb = new StringBuilder();
+                sb.AppendLine(student.ToString());
+                textBox.Text += sb.ToString();
+            }
+        }
+        public static void WriteFile(string path, string strings)
+        {
+            if(!File.Exists(path))
+            {
+                FileStream m = File.Create(path);
+                m.Close();
+                m.Dispose();
+            }
+            StreamWriter sw = new StreamWriter(path);
+            sw.Write(strings);
+            sw.Flush();
+            sw.Close();
+            sw.Dispose();
+            MessageBox.Show("保存成功");
+        }
+        public static void ReadFile(string path, TextBox textBox)
+        {
+            if(!File.Exists(path))
+            {
+                MessageBox.Show("文件不存在");
+            }
+            else
+            {
+                StreamReader sr = new StreamReader(path);
+                string text = sr.ReadToEnd();
+                sr.Close();
+                sr.Dispose();
+                textBox.Text = text;
+            }
+        }
+
+        public static void WriteXML<T>(T list, string path)
+        {
+            System.Xml.Serialization.XmlSerializer writer =
+                new System.Xml.Serialization.XmlSerializer(typeof(T));
+            FileStream file;
+            if (!File.Exists(path))
+            {
+                file = System.IO.File.Create(path);
+            }
+            else
+            {
+                file = File.OpenWrite(path);
+            }
+
+            writer.Serialize(file, list);
+            file.Close();
+        }
+
+        public void ReadXML<T>(ref T list, string path)
+        {
+            System.Xml.Serialization.XmlSerializer reader =
+                new System.Xml.Serialization.XmlSerializer(typeof(T));
+            StreamReader file;
+            if (File.Exists(path))
+            {
+                file = new System.IO.StreamReader(path);
+                list = (T)reader.Deserialize(file);
+                file.Close();
+            }
+            else
+            {
+                MessageBox.Show("文件不存在");
+            }
+        }
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            string path = "chengji.txt";
+            string xmlpath = "xuesheng.xml";
+            WriteXML(students, xmlpath);
+            WriteFile(path, textBox.Text);
+
+        }
+
+        private void button2_Click(object sender, RoutedEventArgs e)
+        {
+            string path = "chengji.txt";
+            string xmlpath = "xuesheng.xml";
+            ReadXML(ref students, xmlpath);
+            ReadFile(path, textBox);
+        }
+
+        private void search_Click(object sender, RoutedEventArgs e)
+        {
+            if(radioButton1.IsChecked == true)
+            {
+                var q = from i in students
+                        where i.Sno.ToString() == textbox1.Text
+                        select i;
+                if(q.Count() == 0)
+                {
+                    MessageBox.Show("没有该学生");
+                }
+                else
+                {
+                    textbox2.Text = q.First().Course.ToString() + "," + q.First().Score.ToString();
+                }
+            }
+        }
+    }
+}
---
+++ 2015exam/Student.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2015exam
+{
+    public class Student
+    {
+        public Int64 Sno { get; set; }
+        public string Name { get; set; }
+        public string Course { get; set; }
+        public int Score { get; set; }
+        public Student(Int64 Sno, string Name, string Course, int Score)
+        {
+            this.Sno = Sno;
+            this.Name = Name;
+            this.Course = Course;
+            this.Score = Score;
+        }
+        public Student()
+        {
+            new Student(0, "未命名", "无", 0);
+        }
+        
+        public override string ToString()
+        {
+            string str = this.Sno.ToString() + "，" + this.Name.ToString() + "，" + this.Course.ToString() + "，" + this.Score.ToString();
+            return str;
+        }
+
+        public static double Avg(List<Student> students)
+        {
+            double sum = 0;
+            int count = 0;
+            foreach(var student in students)
+            {
+                sum += student.Score;
+                count++;
+            }
+            return sum / count;
+        }
+    }
+}
